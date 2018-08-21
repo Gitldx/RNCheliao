737e3de66a9991b546a92b6f98a98227ee49e7cb
ldx <ldx@hello.com>
2018-08-21 11:27:43 +0800

    react-native-navigation init

--- CarChat/index.js
+++ CarChat/index.js
@@ -4,4 +4,29 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+import { Navigation } from 'react-native-navigation'
+
+import { registerScreens } from './screens';
+
+registerScreens();
+
+// AppRegistry.registerComponent(appName, () => App);
+
+Navigation.startTabBasedApp({
+    tabs: [
+      {
+        label: 'One',
+        screen: 'example.FirstTabScreen', // this is a registered name for a screen
+        icon: require('./img/one.png'),
+        // selectedIcon: require('../img/one_selected.png'), // iOS only
+        title: 'Screen One'
+      },
+      {
+        label: 'Two',
+        screen: 'example.FirstTabScreen',
+        icon: require('./img/two.png'),
+        // selectedIcon: require('../img/two_selected.png'), // iOS only
+        title: 'Screen Two'
+      }
+    ]
+  });
